b6e976de5b8e86266236c6651fd4a0f73a562f8b
Wade Barnes <wade.barnes@shaw.ca>
2019-08-27 07:21:04 -0700

    Update properties
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- jenkins/frontend/pipeline/config.groovy
+++ jenkins/frontend/pipeline/config.groovy
@@ -5,31 +5,31 @@
 WAIT_TIMEOUT = 10
 
 // Source repository configuration
-SOURCE_REPO = "https://github.com/bcgov/indy-catalyst.git"
-SOURCE_REPO_BRANCH = "master"
-SOURCE_REPO_CONTEXT_DIRECTORY = "credential-registry/client/tob-web/"
-SOURCE_REPO_WORKING_DIRECTORY = "indy-catalyst"
-SOURCE_REPO_THEME_DIRECTORY = "credential-registry/client/tob-web/src/themes/"
+SOURCE_REPO = https://github.com/bcgov/indy-catalyst.git
+SOURCE_REPO_BRANCH = master
+SOURCE_REPO_CONTEXT_DIRECTORY = credential-registry/client/tob-web/
+SOURCE_REPO_WORKING_DIRECTORY = indy-catalyst
+SOURCE_REPO_THEME_DIRECTORY = credential-registry/client/tob-web/src/themes/
 
 // Theme repository configuration
-THEME_REPO = "https://github.com/bcgov/indy-catalyst.git"
-THEME_REPO_BRANCH = "master"
-THEME_REPO_CONTEXT_DIRECTORY = "starter-kits/credential-registry/client/tob-web/themes/"
-THEME_REPO_WORKING_DIRECTORY = "themes"
+THEME_REPO = https://github.com/bcgov/indy-catalyst.git
+THEME_REPO_BRANCH = master
+THEME_REPO_CONTEXT_DIRECTORY = starter-kits/credential-registry/client/tob-web/themes/
+THEME_REPO_WORKING_DIRECTORY = themes
 
 // Theme configuration
-TOB_THEME_NAME = "bcgov"
+TOB_THEME_NAME = bcgov
 
 // Build configuration
-APP_NAME = "frontend"
-ARTIFACTS_NAME = "${APP_NAME}-artifacts"
-RUNTIME_NAME = "${APP_NAME}-runtime"
+APP_NAME = frontend
+ARTIFACTS_NAME = ${APP_NAME}-artifacts
+RUNTIME_NAME = ${APP_NAME}-runtime
 
 // Deployment Environment TAGs
 DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
 
 // The name of the project namespace(s).
-NAME_SPACE = 'devex-von-bc-tob'
+NAME_SPACE = devex-von-bc-tob
 
 // Instance Suffix
-SUFFIX = '-indy-cat'
+SUFFIX = -indy-cat

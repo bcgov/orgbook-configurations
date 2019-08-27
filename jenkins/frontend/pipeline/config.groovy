ee0a2aa83c6f8bfb90c1d9e76c0a1cbbcafd4542
Wade Barnes <wade.barnes@shaw.ca>
2019-08-27 08:07:47 -0700

    Testing scripts
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- jenkins/frontend/pipeline/config.groovy
+++ jenkins/frontend/pipeline/config.groovy
@@ -1,43 +1,36 @@
-// Pipeline Configuration
+// Utility functions
 // Import this file into another pipeline using 'load'.
+class config {
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
 
-// Wait timeout in minutes
-@groovy.transform.Field
-def WAIT_TIMEOUT = 10
+  // Source repository configuration
+  public static final String SOURCE_REPO = "https://github.com/bcgov/indy-catalyst.git"
+  public static final String  SOURCE_REPO_BRANCH = "master"
+  public static final String  SOURCE_REPO_CONTEXT_DIRECTORY = "credential-registry/client/tob-web/"
+  public static final String  SOURCE_REPO_WORKING_DIRECTORY = "indy-catalyst"
+  public static final String  SOURCE_REPO_THEME_DIRECTORY = "credential-registry/client/tob-web/src/themes/"
 
-// Source repository configuration
-@groovy.transform.Field
-def SOURCE_REPO = "https://github.com/bcgov/indy-catalyst.git"
-@groovy.transform.Field
-def SOURCE_REPO_BRANCH = "master"
-@groovy.transform.Field
-def SOURCE_REPO_CONTEXT_DIRECTORY = "credential-registry/client/tob-web/"
-@groovy.transform.Field
-def SOURCE_REPO_WORKING_DIRECTORY = "indy-catalyst"
-@groovy.transform.Field
-def SOURCE_REPO_THEME_DIRECTORY = "credential-registry/client/tob-web/src/themes/"
+  // Theme repository configuration
+  public static final String  THEME_REPO = "https://github.com/bcgov/indy-catalyst.git"
+  public static final String  THEME_REPO_BRANCH = "master"
+  public static final String  THEME_REPO_CONTEXT_DIRECTORY = "starter-kits/credential-registry/client/tob-web/themes/"
+  public static final String  THEME_REPO_WORKING_DIRECTORY = "themes"
 
-// Theme repository configuration
-def THEME_REPO = "https://github.com/bcgov/indy-catalyst.git"
-def THEME_REPO_BRANCH = "master"
-def THEME_REPO_CONTEXT_DIRECTORY = "starter-kits/credential-registry/client/tob-web/themes/"
-def THEME_REPO_WORKING_DIRECTORY = "themes"
+  // Theme configuration
+  public static final String  TOB_THEME_NAME = "bcgov"
 
-// Theme configuration
-def TOB_THEME_NAME = "bcgov"
+  // Build configuration
+  public static final String  APP_NAME = "frontend"
+  public static final String  ARTIFACTS_NAME = "${APP_NAME}-artifacts"
+  public static final String  RUNTIME_NAME = "${APP_NAME}-runtime"
 
-// Build configuration
-def APP_NAME = "frontend"
-def ARTIFACTS_NAME = "${APP_NAME}-artifacts"
-def RUNTIME_NAME = "${APP_NAME}-runtime"
+  // Deployment Environment TAGs
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
 
-// Deployment Environment TAGs
-def DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = 'devex-von-bc-tob'
 
-// The name of the project namespace(s).
-def NAME_SPACE = 'devex-von-bc-tob'
-
-// Instance Suffix
-def SUFFIX = '-indy-cat'
-
-return this;
+  // Instance Suffix
+  public static final String  SUFFIX = '-indy-cat'
+}

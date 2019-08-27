d55ee7d429f9022dafb438a876b65b9fa046dc03
Wade Barnes <wade.barnes@shaw.ca>
2019-08-27 06:56:03 -0700

    Convert config variables to properties.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- jenkins/frontend/pipeline/config.groovy
+++ jenkins/frontend/pipeline/config.groovy
@@ -2,36 +2,36 @@
 // Import this file into another pipeline using 'load'.
 
 // Wait timeout in minutes
-def WAIT_TIMEOUT = 10
+public static final int WAIT_TIMEOUT = 10
 
 // Source repository configuration
-def SOURCE_REPO = "https://github.com/bcgov/indy-catalyst.git"
-def SOURCE_REPO_BRANCH = "master"
-def SOURCE_REPO_CONTEXT_DIRECTORY = "credential-registry/client/tob-web/"
-def SOURCE_REPO_WORKING_DIRECTORY = "indy-catalyst"
-def SOURCE_REPO_THEME_DIRECTORY = "credential-registry/client/tob-web/src/themes/"
+public static final String SOURCE_REPO = "https://github.com/bcgov/indy-catalyst.git"
+public static final String  SOURCE_REPO_BRANCH = "master"
+public static final String  SOURCE_REPO_CONTEXT_DIRECTORY = "credential-registry/client/tob-web/"
+public static final String  SOURCE_REPO_WORKING_DIRECTORY = "indy-catalyst"
+public static final String  SOURCE_REPO_THEME_DIRECTORY = "credential-registry/client/tob-web/src/themes/"
 
 // Theme repository configuration
-def THEME_REPO = "https://github.com/bcgov/indy-catalyst.git"
-def THEME_REPO_BRANCH = "master"
-def THEME_REPO_CONTEXT_DIRECTORY = "starter-kits/credential-registry/client/tob-web/themes/"
-def THEME_REPO_WORKING_DIRECTORY = "themes"
+public static final String  THEME_REPO = "https://github.com/bcgov/indy-catalyst.git"
+public static final String  THEME_REPO_BRANCH = "master"
+public static final String  THEME_REPO_CONTEXT_DIRECTORY = "starter-kits/credential-registry/client/tob-web/themes/"
+public static final String  THEME_REPO_WORKING_DIRECTORY = "themes"
 
 // Theme configuration
-def TOB_THEME_NAME = "bcgov"
+public static final String  TOB_THEME_NAME = "bcgov"
 
 // Build configuration
-def APP_NAME = "frontend"
-def ARTIFACTS_NAME = "${APP_NAME}-artifacts"
-def RUNTIME_NAME = "${APP_NAME}-runtime"
+public static final String  APP_NAME = "frontend"
+public static final String  ARTIFACTS_NAME = "${APP_NAME}-artifacts"
+public static final String  RUNTIME_NAME = "${APP_NAME}-runtime"
 
 // Deployment Environment TAGs
-def DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
 
 // The name of the project namespace(s).
-def NAME_SPACE = 'devex-von-bc-tob'
+public static final String  NAME_SPACE = 'devex-von-bc-tob'
 
 // Instance Suffix
-def SUFFIX = '-indy-cat'
+public static final String  SUFFIX = '-indy-cat'
 
 return this;

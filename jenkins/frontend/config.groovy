3e30a46325a091c5bbacc0f580fdc5a8170bedf3
Wade Barnes <wade.barnes@shaw.ca>
2019-08-27 09:56:10 -0700

    Add frontend build/deploy pipeline.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ jenkins/frontend/config.groovy
@@ -0,0 +1,38 @@
+// Pipeline Configuration Properties
+// Import this file into the pipeline using 'load'.
+class config {
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
+
+  // Source repository configuration
+  public static final String SOURCE_REPO = "https://github.com/bcgov/indy-catalyst.git"
+  public static final String  SOURCE_REPO_BRANCH = "master"
+  public static final String  SOURCE_REPO_CONTEXT_DIRECTORY = "credential-registry/client/tob-web/"
+  public static final String  SOURCE_REPO_WORKING_DIRECTORY = "indy-catalyst"
+  public static final String  SOURCE_REPO_THEME_DIRECTORY = "credential-registry/client/tob-web/src/themes/"
+
+  // Theme repository configuration
+  public static final String  THEME_REPO = "https://github.com/bcgov/indy-catalyst.git"
+  public static final String  THEME_REPO_BRANCH = "master"
+  public static final String  THEME_REPO_CONTEXT_DIRECTORY = "starter-kits/credential-registry/client/tob-web/themes/"
+  public static final String  THEME_REPO_WORKING_DIRECTORY = "themes"
+
+  // Theme configuration
+  public static final String  TOB_THEME_NAME = "bcgov"
+
+  // Build configuration
+  public static final String  APP_NAME = "frontend"
+  public static String  ARTIFACTS_NAME = "${this.APP_NAME}-artifacts"
+  public static String  RUNTIME_NAME = "${this.APP_NAME}-runtime"
+
+  // Deployment Environment TAGs
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = 'devex-von-bc-tob'
+
+  // Instance Suffix
+  public static final String  SUFFIX = '-indy-cat'
+}
+
+return new config();

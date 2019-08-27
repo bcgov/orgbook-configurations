306e3a5312f489984e234fc4641554f81624056e
Wade Barnes <wade.barnes@shaw.ca>
2019-08-27 09:53:53 -0700

    Add agent build/deploy pipeline.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ jenkins/agent/config.groovy
@@ -0,0 +1,22 @@
+// Pipeline Configuration Properties
+// Import this file into the pipeline using 'load'.
+class config {
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
+
+  // Build configuration
+  public static final String  APP_NAME = "agent"
+  public static String BASE_NAME = "${this.APP_NAME}-base"
+  public static final String[] BUILDS = ["${this.APP_NAME}", "${this.BASE_NAME}"]
+
+  // Deployment Environment TAGs
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = 'devex-von-bc-tob'
+
+  // Instance Suffix
+  public static final String  SUFFIX = '-indy-cat'
+}
+
+return new config();

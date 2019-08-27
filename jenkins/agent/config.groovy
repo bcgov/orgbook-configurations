c1d8156a3d2fa49de6dbdb487a35d5aa04312517
Wade Barnes <wade.barnes@shaw.ca>
2019-08-27 10:59:34 -0700

    Unwind tests
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- jenkins/agent/config.groovy
+++ jenkins/agent/config.groovy
@@ -1,24 +1,23 @@
-import ../baseConfig;
-
 // Pipeline Configuration Properties
 // Import this file into the pipeline using 'load'.
-class config extends baseConfig {
-  // // Wait timeout in minutes
-  // public static final int WAIT_TIMEOUT = 10
+
+class config {
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
 
   // Build configuration
   public static final String  APP_NAME = "agent"
   public static String BASE_NAME = "${this.APP_NAME}-base"
   public static final String[] BUILDS = ["${this.BASE_NAME}", "${this.APP_NAME}"]
 
-  // // Deployment Environment TAGs
-  // public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+  // Deployment Environment TAGs
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
 
-  // // The name of the project namespace(s).
-  // public static final String  NAME_SPACE = 'devex-von-bc-tob'
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = 'devex-von-bc-tob'
 
-  // // Instance Suffix
-  // public static final String  SUFFIX = '-indy-cat'
+  // Instance Suffix
+  public static final String  SUFFIX = '-indy-cat'
 }
 
 return new config();

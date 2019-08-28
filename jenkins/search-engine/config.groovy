de013e873c7dbebc6351cb2c2ad242cd39ec5e1f
Wade Barnes <wade.barnes@shaw.ca>
2019-08-28 10:38:04 -0700

    Update search-engine build/deploy pipeline.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- jenkins/search-engine/config.groovy
+++ jenkins/search-engine/config.groovy
@@ -2,7 +2,7 @@
 // Import this file into the pipeline using 'load'.
 class config extends orgbook.baseConfig {
   // Build configuration
-  public static final String  APP_NAME = "agent"
+  public static final String  APP_NAME = "search-engine"
   public static String BASE_NAME = "${this.APP_NAME}-base"
   public static final String[] BUILDS = ["${this.BASE_NAME}", "${this.APP_NAME}"]
 }

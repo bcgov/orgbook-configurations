407686259b5a9a9e00b9f464e0fad43d7d98e202
Wade Barnes <wade.barnes@shaw.ca>
2019-08-28 09:10:40 -0700

    Refactor scripts
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ src/orgbook/libConfig.groovy
@@ -0,0 +1,8 @@
+    
+package orgbook;
+
+class libConfig {
+  public static final String LIB_NAME= "orgbook-lib"
+  public static final String LIB_REPO = "https://github.com/WadeBarnes/orgbook-configurations.git"
+  public static final String LIB_BRANCH = "pipeline-refactor"
+}

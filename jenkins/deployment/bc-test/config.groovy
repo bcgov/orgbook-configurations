2bc3046b6908c58640e0d903469b88d643a4a770
Wade Barnes <wade.barnes@shaw.ca>
2019-11-08 08:32:41 -0800

    Update pipeline config
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- jenkins/deployment/bc-test/config.groovy
+++ jenkins/deployment/bc-test/config.groovy
@@ -2,8 +2,8 @@
 // Import this file into the pipeline using 'load'.
 class config extends bc.baseConfig {
   // Deployment configuration
-  public static final String SOURCE_TAG = "${this.DEPLOYMENT_ENVIRONMENT_TAGS[0]}"
-  public static final String DESTINATION_TAG = "${this.DEPLOYMENT_ENVIRONMENT_TAGS[1]}"
+  public static final String SOURCE_TAG = "${super.DEPLOYMENT_ENVIRONMENT_TAGS[0]}"
+  public static final String DESTINATION_TAG = "${super.DEPLOYMENT_ENVIRONMENT_TAGS[1]}"
 }
 
 return new config();

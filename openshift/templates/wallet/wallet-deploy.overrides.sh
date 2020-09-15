8a0a729b8cf5803ec41060670bec9a20369a994f
Wade Barnes <wade.barnes@shaw.ca>
2020-09-15 12:35:30 -0700

    Add configmap overlay for wallet logging config
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/templates/wallet/wallet-deploy.overrides.sh
+++ openshift/templates/wallet/wallet-deploy.overrides.sh
@@ -5,12 +5,19 @@ else
   _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
 fi
 
-# ======================================================
+# =====================================================================
 # Special Deployment Parameters needed for DB Deployment
-# ------------------------------------------------------
-# The results need to be encoded as OpenShift template
-# parameters for use with oc process.
-# ======================================================
+# ---------------------------------------------------------------------
+# The generated config map is used to support logging configuration
+# =====================================================================
+LOGGING_CONFIG_MAP_NAME=${LOGGING_CONFIG_MAP_NAME:-wallet-logging-conf-indy-cat}
+SOURCE_FILE=$( dirname "$0" )/../db/config/postgresql-cfg/logging.conf
+
+OUTPUT_FORMAT=json
+OUTPUT_FILE=${LOGGING_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
+
+printStatusMsg "Generating ConfigMap; ${LOGGING_CONFIG_MAP_NAME} ..."
+generateConfigMap "${LOGGING_CONFIG_MAP_NAME}" "${SOURCE_FILE}" "${OUTPUT_FORMAT}" "${OUTPUT_FILE}"
 
 if createOperation; then
   # Randomly generate a set of credentials without asking ...

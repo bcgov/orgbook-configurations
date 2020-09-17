08975312ab99876a96720f13b4d0b749346cdce6
Wade Barnes <wade.barnes@shaw.ca>
2020-09-17 11:38:36 -0700

    Add configmap overlay for db autovacuum config
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/templates/db/db-deploy.overrides.sh
+++ openshift/templates/db/db-deploy.overrides.sh
@@ -5,19 +5,24 @@ else
   _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
 fi
 
-# =====================================================================
+# ===================================================================================
 # Special Deployment Parameters needed for DB Deployment
-# ---------------------------------------------------------------------
-# The generated config map is used to support logging configuration
-# =====================================================================
-LOGGING_CONFIG_MAP_NAME=${LOGGING_CONFIG_MAP_NAME:-db-logging-conf-indy-cat}
+# -----------------------------------------------------------------------------------
+# The generated config maps are used to support logging and autovacuum configuration
+# ==================================================================================
+CONFIG_MAP_NAME=${LOGGING_CONFIG_MAP_NAME:-db-logging-conf-indy-cat}
 SOURCE_FILE=$( dirname "$0" )/config/postgresql-cfg/logging.conf
-
 OUTPUT_FORMAT=json
-OUTPUT_FILE=${LOGGING_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
+OUTPUT_FILE=${CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
+printStatusMsg "Generating ConfigMap; ${CONFIG_MAP_NAME} ..."
+generateConfigMap "${CONFIG_MAP_NAME}" "${SOURCE_FILE}" "${OUTPUT_FORMAT}" "${OUTPUT_FILE}"
 
-printStatusMsg "Generating ConfigMap; ${LOGGING_CONFIG_MAP_NAME} ..."
-generateConfigMap "${LOGGING_CONFIG_MAP_NAME}" "${SOURCE_FILE}" "${OUTPUT_FORMAT}" "${OUTPUT_FILE}"
+CONFIG_MAP_NAME=${AUTOVACUMM_CONFIG_MAP_NAME:-db-autovacuum-conf-indy-cat}
+SOURCE_FILE=$( dirname "$0" )/config/postgresql-cfg/autovacuum.conf
+OUTPUT_FORMAT=json
+OUTPUT_FILE=${CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
+printStatusMsg "Generating ConfigMap; ${CONFIG_MAP_NAME} ..."
+generateConfigMap "${CONFIG_MAP_NAME}" "${SOURCE_FILE}" "${OUTPUT_FORMAT}" "${OUTPUT_FILE}"
 
 if createOperation; then
   # Randomly generate a set of credentials without asking ...

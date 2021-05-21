2f3a543926d94842edf7f20504645137529a0e22
Wade Barnes <wade@neoterictech.ca>
2021-05-21 12:27:54 -0700

    Switch to the bcgov/orgbook-bc-client for the frontend
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- jenkins/frontend/config.groovy
+++ jenkins/frontend/config.groovy
@@ -1,27 +1,11 @@
 // Pipeline Configuration Properties
 // Import this file into the pipeline using 'load'.
 class config extends bc.baseConfig {
-  // Source repository configuration
-  public static final String SOURCE_REPO = "https://github.com/bcgov/aries-vcr.git"
-  public static final String  SOURCE_REPO_BRANCH = "master"
-  public static final String  SOURCE_REPO_CONTEXT_DIRECTORY = "client/"
-  public static final String  SOURCE_REPO_WORKING_DIRECTORY = "indy-catalyst"
-  public static final String  SOURCE_REPO_THEME_DIRECTORY = "client/themes/"
-
-  // Theme repository configuration
-  public static final String  THEME_REPO = "https://github.com/bcgov/aries-vcr.git"
-  public static final String  THEME_REPO_BRANCH = "master"
-  public static final String  THEME_REPO_CONTEXT_DIRECTORY = "client/themes/"
-  public static final String  THEME_REPO_WORKING_DIRECTORY = "themes"
-
-  // Theme configuration
-  public static final String  THEME_NAME = "bcgov"
-
   // Build configuration
   public static final String  APP_NAME = "frontend"
   public static String  ARTIFACTS_NAME = "${this.APP_NAME}-artifacts"
   public static String  RUNTIME_NAME = "${this.APP_NAME}-runtime"
-  public static final String[] BUILDS = ["${this.RUNTIME_NAME}", "${this.APP_NAME}"]
+  public static final String[] BUILDS = ["${this.RUNTIME_NAME}", "${this.ARTIFACTS_NAME}", "${this.APP_NAME}"]
 }
 
 return new config();

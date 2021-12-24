821a781b0d284e897fb914c9be00da9c922d5885
Wade Barnes <wade@neoterictech.ca>
2021-12-24 14:33:57 -0800

    Update git references.
    - The default branch of https://github.com/bcgov/aries-vcr.git has been renamed to `main`.
    - Setting `GIT_REF` to a blank string tells the build to use the repo's default branch.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,6 +1,6 @@
 export PROJECT_NAMESPACE="8ad0ea"
 export GIT_URI="https://github.com/bcgov/aries-vcr.git"
-export GIT_REF="master"
+export GIT_REF="main"
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
